a07fa6b8046906cae355d70e29c2746edafc10fa
Hugo Alexandre <hugobrumado@hotmail.com>
2024-03-12 15:29:53 -0300

    Fix bug in login functionality

--- poloniex.py
+++ poloniex.py
@@ -1,4 +1,7 @@
-import requests
+import itertools
+import sys
+import threading
+
 import time
 import json
 from websocket import create_connection, WebSocketConnectionClosedException
@@ -7,64 +10,21 @@ from arbitrage import CryptoArbitrage
 class Poloniex:
 
     def __init__(self):
-        self.pricesUrl = 'https://api.poloniex.com/markets/price'
         self.websocketUrl = 'wss://ws.poloniex.com/ws/public'
         self.graph = {}
-        self.transactionFee = 0.002
+        self.transactionFee = 0.2/100  # Taxa de transação de 0.2%
 
-    def getPrices(self):
-        response = requests.get(self.pricesUrl).json()
-        tsLimit = self.minimumTimestamp()
-        tickerPrices = []
-        for item in response:
-            if item['ts'] > tsLimit:
-                tickerPrices.append({'symbol': item['symbol'], 'price': float(item['price'])})
-        return tickerPrices
-    
-    def minimumTimestamp(self):
-        return time.time()*1000 - 450000  # Ajustado para 7 minutos atrás
+    def spinner(self, message="Procurando oportunidades"):
+        """
+        Gera um spinner de console ao lado de uma mensagem.
+        """
+        for spin_char in itertools.cycle(['-', '\\', '|', '/']):
+            status = f"\r{message} {spin_char}"
+            sys.stdout.write(status)
+            sys.stdout.flush()
+            time.sleep(0.1)
 
-    def getGraph(self):
-        tickerPrices = self.getPrices()
-        for item in tickerPrices:
-            base, quote = item['symbol'].split('_')
-            price = item['price'] * (1 - self.transactionFee)
-            if base not in self.graph:
-                self.graph[base] = {}
-            if quote not in self.graph:
-                self.graph[quote] = {}
-            self.graph[base][quote] = price
-            self.graph[quote][base] = 1 / price
-
-    def checkArbitrageOpportunity(self):
-        arb = CryptoArbitrage(self.graph)
-        negative_cycle = arb.find_negative_cycle()
-        if negative_cycle:
-            profit = arb.calculate_profit(negative_cycle)
-            if profit > 1.05:
-                print(f'\nCycle Time {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
-                print(f"Positive cycle: {negative_cycle}")
-                print(f"% Profit: {profit}\n")
-                with open('arbitrage.txt', 'a') as f:
-                    f.write(f'Cycle Time {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}\n')
-                    f.write(f"Positive cycle: {negative_cycle}\n")
-                    f.write(f"% Profit: {profit}\n\n")
-            # arb.plot_cycle_graph(negative_cycle)  # Opcional, se você quiser visualizar o ciclo
-        else:
-            print(f'Cycle Time {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
-            print("No arbitrage opportunity found.\n")
-            
-    def updateGraphWithClosePrice(self, symbol, close_price):
-        base, quote = symbol.split('_')
-        price = close_price * (1 - self.transactionFee)
-        if base not in self.graph:
-            self.graph[base] = {}
-        if quote not in self.graph:
-            self.graph[quote] = {}
-        self.graph[base][quote] = price
-        self.graph[quote][base] = 1 / price
-    
-    def getCurrentSymbols(self):
+    def _getCurrentSymbols(self):
         payload = {
                 "event": "subscribe",
                 "channel": ["Ticker"],
@@ -81,57 +41,114 @@ class Poloniex:
         ws.close()
         return symbols
 
-    def subscribeTicker(self, symbols=["all"]):
-        payload = {
-            "event": "subscribe",
-            "channel": ["Ticker"],
-            "symbols": symbols 
-        }
+    def _buildEmptyGraph(self):
+        symbols = self._getCurrentSymbols()
+        for symbol in symbols:
+            base, quote = symbol.split('_')
+            if base not in self.graph:
+                self.graph[base] = {}
+            if quote not in self.graph:
+                self.graph[quote] = {}
+            self.graph[base][quote] = 1
+            self.graph[quote][base] = 1
 
-        while True:  # Loop infinito para tentar reconectar sempre
-            try:
-                ws = create_connection(self.websocketUrl)
-                ws.send(json.dumps(payload))
-                while True:
-                    response = json.loads(ws.recv())
-                    if "data" in response:  # Verifica se a chave 'data' está na resposta
-                        for data in response["data"]:
-                            symbol = data["symbol"]
-                            close_price = float(data["close"])
-                            self.updateGraphWithClosePrice(symbol, close_price)
-                        self.checkArbitrageOpportunity()
-                    time.sleep(1)
+    def _cleanGraph(self, currentSymbols):
+        """Remove os nós obsoletos do grafo."""
+        currentBases = set([symbol.split('_')[0] for symbol in currentSymbols])
+        currentQuotes = set([symbol.split('_')[1] for symbol in currentSymbols])
+        allCurrentSymbols = currentBases.union(currentQuotes)
 
-            except WebSocketConnectionClosedException as e:
-                print("WebSocket connection closed, trying to reconnect...")
-                time.sleep(2)  # Espera um pouco antes de tentar reconectar
+        # Encontra os nós no grafo que não estão mais presentes nos símbolos atuais
+        nodesToRemove = set(self.graph.keys()) - allCurrentSymbols
+        
+        # Remove os nós obsoletos
+        for node in nodesToRemove:
+            del self.graph[node]
 
-            except Exception as e:
-                print(f"An error occurred: {e}")
-                ws.close()
-                break  # Sai do loop após um erro desconhecido
-    
+        # Além disso, remove as conexões obsoletas
+        for base in list(self.graph.keys()):
+            for quote in list(self.graph[base].keys()):
+                if f"{base}_{quote}" not in currentSymbols and f"{quote}_{base}" not in currentSymbols:
+                    del self.graph[base][quote]
+
+    def _updateGraph(self, orderBookData):
+        for data in orderBookData:
+            symbol = data['symbol']
+            if symbol == "BTC_USDT":
+                print("Ignoring BTC_USDT")
+
+            base, quote = symbol.split('_')
+            asks = data['asks']
+            bids = data['bids']
+
+            if len(asks) == 0 or len(bids) == 0:
+                continue
+
+            # Assume que o primeiro 'ask' e o primeiro 'bid' são os melhores preços disponíveis
+            bestAskPrice = float(asks[0][0]) * (1 + self.transactionFee)  # Adiciona a taxa de transação ao preço de compra
+            bestBidPrice = float(bids[0][0]) * (1 - self.transactionFee)  # Subtrai a taxa de transação do preço de venda
+
+            # Atualiza o grafo com os preços de 'ask' e 'bid'
+            if base not in self.graph:
+                self.graph[base] = {}
+            if quote not in self.graph:
+                self.graph[quote] = {}
+            
+            # Aqui, estamos usando o preço de 'bid' para a conversão de base para quote
+            # e o preço de 'ask' para a conversão de quote para base
+            self.graph[base][quote] = bestBidPrice
+            self.graph[quote][base] = 1 / bestAskPrice
+
+    def checkArbitrageOpportunity(self):
+        arb = CryptoArbitrage(self.graph)
+        negative_cycle = arb.find_negative_cycle()
+        if negative_cycle:
+            profit = arb.calculate_profit(negative_cycle)
+            if profit > 1.05:
+                print(f'\nCycle Time {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}')
+                print(f"Positive cycle: {negative_cycle}")
+                print(f"% Profit: {profit}\n")
+                with open('arbitrage.txt', 'a') as f:
+                    f.write(f'Cycle Time {time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())}\n')
+                    f.write(f"Positive cycle: {negative_cycle}\n")
+                    f.write(f"% Profit: {profit}\n\n")
+            # arb.plot_cycle_graph(negative_cycle)  # Opcional, se você quiser visualizar o ciclo
+        else:
+            sys.stdout.write('.')
+            sys.stdout.flush()
+
     def subscribeOrderBook(self, symbols=["BTC_USDT"]):
         payload = {
             "event": "subscribe",
             "channel": ["book"],
             "symbols": symbols 
         }
+        end_time = time.time() + 60  # Define um tempo de execução de 60 segundos
 
-        while True:
+        while time.time() < end_time:
             try:
                 ws = create_connection(self.websocketUrl)
                 ws.send(json.dumps(payload))
-                while True:
+                while time.time() < end_time:
                     response = json.loads(ws.recv())
                     if "data" in response:
-                        print(response)
-                    time.sleep(1)
+                        self._updateGraph(response['data'])
+                        self.checkArbitrageOpportunity()
 
             except WebSocketConnectionClosedException as e:
                 print("WebSocket connection closed, trying to reconnect...")
                 time.sleep(2)
-        
+    
+    def run(self):
+        while True:
+            sys.stdout.write("\033[K")  # Limpa a linha do console
+            currentSymbols = self._getCurrentSymbols()
+            self._cleanGraph(currentSymbols)
+            
+            thread = threading.Thread(target=self.subscribeOrderBook, args=(currentSymbols,))
+            thread.start()
+            thread.join()
+
 if __name__ == '__main__':
     poloniex = Poloniex()
-    poloniex.subscribeOrderBook(['BTS_BTC', 'DASH_BTC', 'DOGE_BTC'])
+    poloniex.run()
